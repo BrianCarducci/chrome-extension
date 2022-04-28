349d8809d047b5f55f57e27ed82f6fcff0029815
Brian Carducci <briancarducci@gmail.com>
2022-04-27 20:27:22 -0400

    yeo

--- yo.js
+++ yo.js
@@ -1,6 +1,4 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    // chrome.storage.sync.set({ color });
+    console.log('yeooooooo');
 });
