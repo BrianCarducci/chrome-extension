27bad436f8ff21ffc72463bb20dffe15c86d1731
Brian Carducci <briancarducci@gmail.com>
2022-05-03 23:04:05 -0400

    trying diff ways to get followed channel div

--- yo.js
+++ yo.js
@@ -4,11 +4,61 @@ var categories = ['favz'];
 
 
 
-var xpathExpression = "//div[contains(@aria-label, 'Followed Channels')]";
+// var xpathExpression = "//div[contains(@aria-label, 'Followed Channels')]";
 
-var xpathResult = new XPathEvaluator()
-    .createExpression(xpathExpression)
-    .evaluate(document, XPathResult.FIRST_ORDERED_NODE_TYPE)
-    .singleNodeValue;
+// var xpathResult = new XPathEvaluator()
+//     .createExpression(xpathExpression)
+//     .evaluate(document, XPathResult.FIRST_ORDERED_NODE_TYPE)
+//     .singleNodeValue;
 
-console.log(xpathResult);
+// console.log(xpathResult);
+
+// const x = document.querySelector('[aria-label="Followed Channels"]')
+// x = document.getElementsByTagName('div')
+// const sideNav = document.querySelector('[id="sideNav"]');
+
+// let selectFunc = async () => {
+//     let sideNav = document.querySelector('div[aria-label="Followed Channels"]');;
+//     do {
+//         setTimeout(() => {
+//             sideNav = document.querySelector('div[aria-label="Followed Channels"]');
+//         }, 250);
+//     } while (!sideNav);
+// };
+
+// selectFunc();
+
+// console.log(sideNav)
+
+// let followedChannelsPromise;
+// intervalId = setInterval(() => {
+//     followedChannelsPromise = new Promise((resolve, reject) => {
+//         let followedChannelsDiv = document.querySelector('div[aria-label="Followed Channels"]');
+//         if (followedChannelsDiv) {
+//             clearInterval(intervalId);
+//             resolve(followedChannelsDiv);
+//         } else {
+//             reject();
+//         }
+//     });
+// }, 500);
+
+// (async () => {
+//     await 
+// })()
+
+
+
+
+// let followedChannelsDiv;
+// intervalId = setInterval(() => {
+//     followedChannelsDiv = document.querySelector('div[aria-label="Followed Channels"]');
+//     if (followedChannelsDiv) {
+//         clearInterval(intervalId);
+//     }
+// }, 500);
+
+window.onload = () => {
+    followedChannelsDiv = document.querySelector('div[aria-label="Followed Channels"]');
+    console.log(followedChannelsDiv)
+}
