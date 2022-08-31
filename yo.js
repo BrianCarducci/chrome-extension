e0572fe1c824fc0de14b7f9c451a8f39d80eb1b0
Brian Carducci <briancarducci@gmail.com>
2022-08-31 12:36:21 -0400

    now injecting channel divs into favs div

--- yo.js
+++ yo.js
@@ -1,64 +1,41 @@
-var users = ['xqcow', 'avoidingthepuddle'];
+var favs = [
+  "xqc",
+  "avoidingthepuddle",
+  "distortion2",
+  "trainwreckstv",
+  "miirio",
+  "orchlon",
+  "shroud",
+  "sodapoppin",
+];
 
-var categories = ['favz'];
-
-
-
-// var xpathExpression = "//div[contains(@aria-label, 'Followed Channels')]";
-
-// var xpathResult = new XPathEvaluator()
-//     .createExpression(xpathExpression)
-//     .evaluate(document, XPathResult.FIRST_ORDERED_NODE_TYPE)
-//     .singleNodeValue;
-
-// console.log(xpathResult);
-
-// const x = document.querySelector('[aria-label="Followed Channels"]')
-// x = document.getElementsByTagName('div')
-// const sideNav = document.querySelector('[id="sideNav"]');
-
-// let selectFunc = async () => {
-//     let sideNav = document.querySelector('div[aria-label="Followed Channels"]');;
-//     do {
-//         setTimeout(() => {
-//             sideNav = document.querySelector('div[aria-label="Followed Channels"]');
-//         }, 250);
-//     } while (!sideNav);
-// };
-
-// selectFunc();
-
-// console.log(sideNav)
-
-// let followedChannelsPromise;
-// intervalId = setInterval(() => {
-//     followedChannelsPromise = new Promise((resolve, reject) => {
-//         let followedChannelsDiv = document.querySelector('div[aria-label="Followed Channels"]');
-//         if (followedChannelsDiv) {
-//             clearInterval(intervalId);
-//             resolve(followedChannelsDiv);
-//         } else {
-//             reject();
-//         }
-//     });
-// }, 500);
-
-// (async () => {
-//     await 
-// })()
-
-
-
-
-// let followedChannelsDiv;
-// intervalId = setInterval(() => {
-//     followedChannelsDiv = document.querySelector('div[aria-label="Followed Channels"]');
-//     if (followedChannelsDiv) {
-//         clearInterval(intervalId);
-//     }
-// }, 500);
+var categories = ["favz"];
 
 window.onload = () => {
-    followedChannelsDiv = document.querySelector('div[aria-label="Followed Channels"]');
-    console.log(followedChannelsDiv)
-}
+  followedChannelsDiv = document.querySelector(
+    'div[aria-label="Followed Channels"]'
+  );
+
+  const favsDiv = document.createElement("div");
+  const favsHeader = document.createElement("h2");
+
+  favsHeader.innerHTML = "FAVORITES";
+  favsDiv.append(favsHeader);
+
+  for (fav of favs) {
+    twitchUserDiv = document.querySelector(`[href="/${fav}"]`);
+    if (twitchUserDiv) {
+      twitchUserDivClone = twitchUserDiv.cloneNode(true);
+      favsDiv.append(twitchUserDivClone);
+    } else {
+    }
+  }
+
+  followedChannelsDiv.prepend(favsDiv);
+};
+
+// CHANNEL DIV EXAMPLE
+/* <a data-a-id="followed-channel-43" data-test-selector="followed-channel" class="ScCoreLink-sc-udwpw5-0 cmQKL InjectLayout-sc-588ddc-0 hqHHYw side-nav-card__link tw-link" href="/avoidingthepuddle"><div class="Layout-sc-nxg1ff-0 kZFVrV side-nav-card__avatar side-nav-card__avatar--offline"><figure aria-label="AvoidingThePuddle" class="ScAvatar-sc-12nlgut-0 dncwPH tw-avatar"><img class="InjectLayout-sc-588ddc-0 iDjrEF tw-image tw-image-avatar" alt="AvoidingThePuddle" src="https://static-cdn.jtvnw.net/jtv_user_pictures/avoidingthepuddle-profile_image-8656268d189524ec-70x70.jpeg"></figure></div><div class="Layout-sc-nxg1ff-0 blhocS"><div data-a-target="side-nav-card-metadata" class="Layout-sc-nxg1ff-0 bGPqDX"><div class="Layout-sc-nxg1ff-0 gcwIMz side-nav-card__title"><p title="AvoidingThePuddle" data-a-target="side-nav-title" class="CoreText-sc-cpl358-0 gYupEs InjectLayout-sc-588ddc-0 emHXNr">AvoidingThePuddle</p></div><div class="Layout-sc-nxg1ff-0 bXhxYI side-nav-card__metadata" data-a-target="side-nav-game-title"><p class="CoreText-sc-cpl358-0 ciPVTQ"></p></div></div><div class="Layout-sc-nxg1ff-0 iiA-dIp side-nav-card__live-status" data-a-target="side-nav-live-status"><span class="CoreText-sc-cpl358-0 iUznyJ">Offline</span></div></div></a> */
+
+// FOLLOWED CHANNELS HEADER DIV EXAMPLE
+/* <div class="Layout-sc-nxg1ff-0 hbYWXo side-nav-header" data-a-target="side-nav-header-expanded"><h2 class="CoreText-sc-cpl358-0 ezafKb">Followed Channels</h2></div> */
